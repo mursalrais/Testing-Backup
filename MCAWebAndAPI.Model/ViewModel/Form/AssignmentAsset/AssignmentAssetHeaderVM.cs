2147b49c0226da4caf233fc31c71587315984dcc
yaya123 <anugerah.septian@dac-solution.com>
2016-04-21 09:39:54 +0700

    Modul Assignment Asset

---
+++ MCAWebAndAPI.Model/ViewModel/Form/AssignmentAsset/AssignmentAssetHeaderVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace MCAWebAndAPI.Model.ViewModel.Form.AssignmentAsset
+{
+    public class AssignmentAssetHeaderVM
+    {
+        public string TransactionType { get; set; }
+        public List<string> AssetHolder { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
