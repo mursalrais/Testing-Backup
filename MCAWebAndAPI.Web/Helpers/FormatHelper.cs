aed8400e57bb7bb2525277fb8163b79817003435
Rahadian D Dewandono <gusdewa@gmail.com>
2016-05-09 17:38:41 +0700

    Add format helper

---
+++ MCAWebAndAPI.Web/Helpers/FormatHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace MCAWebAndAPI.Web.Helpers
+{
+    public class FormatHelper
+    {
+        public string ConvertToDigit(int value, int numberOfDigit)
+        {
+            var result = value + string.Empty;
+            for(int index = 0; index < numberOfDigit; index++)
+            {
+                result += 0;
+            }
+
+            result = result.Reverse().ToString();
+            return result;
+        }
+    }
+}
