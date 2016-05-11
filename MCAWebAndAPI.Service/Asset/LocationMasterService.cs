0a82af6b682a180be133722d32c796e7e16b5162
rezaffikri <rezaf.fikri@gmail.com>
2016-05-11 14:15:04 +0700

    Form Location Master

---
+++ MCAWebAndAPI.Service/Asset/LocationMasterService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MCAWebAndAPI.Model.ViewModel.Form.Asset;
+using NLog;
+using MCAWebAndAPI.Service.SPUtil;
+
+namespace MCAWebAndAPI.Service.Asset
+{
+    public class LocationMasterService : ILocationMasterService
+    {
+        string _siteUrl = "https://eceos2.sharepoint.com/sites/mca-dev/dev/";
+        static Logger logger = LogManager.GetCurrentClassLogger();
+        const string SP_LOCATIONMASTER_LISTNAME = "Location Master";
+
+        public bool CreateLocationMaster(LocationMasterVM locationMaster)
+        {
+            var columnValues = new Dictionary<string, object>();
+            columnValues.Add("Title", locationMaster.Header.Remarks);
+            columnValues.Add("FloorName", locationMaster.Header.FloorName);
+            columnValues.Add("RoomName", locationMaster.Header.RoomName);
+            columnValues.Add("OfficeName", locationMaster.Header.OfficeName);
+            try
+            {
+                SPConnector.AddListItem(SP_LOCATIONMASTER_LISTNAME, columnValues, _siteUrl);
+            }
+            catch (Exception e)
+            {
+                logger.Debug(e.Message);
+            }
+            var entitiy = new LocationMasterVM();
+            entitiy = locationMaster;
+            return true;
+        }
+
+        public LocationMasterVM GetLocationMaster()
+        {
+            var viewModel = new LocationMasterVM();
+
+            return viewModel;
+        }
+
+        public void SetSiteUrl(string siteUrl)
+        {
+            _siteUrl = siteUrl;
+        }
+
+        public bool DeleteLocationMasterVM(LocationMasterVM locationMaster)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateLocationMasterVM(LocationMasterVM locationMaster)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MCAWebAndAPI.Web/Controllers/ASSLocationMasterController.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Mvc;
+using MCAWebAndAPI.Model.ViewModel.Form.Asset;
+using MCAWebAndAPI.Service.Asset;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using MCAWebAndAPI.Web.Helpers;
+
+namespace MCAWebAndAPI.Web.Controllers
+{
+    public class ASSLocationMasterController : Controller
+    {
+        ILocationMasterService _locationMasterService;
+
+        public ASSLocationMasterController()
+        {
+            _locationMasterService = new LocationMasterService();
+        }
+
+        // GET: ASSLocationMaster
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = _locationMasterService.GetLocationMaster();
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Submit(LocationMasterVM _data)
+        {
+           _locationMasterService.CreateLocationMaster(_data);
+            return this.Jsonp(_data);
+        }
+    }
+}
