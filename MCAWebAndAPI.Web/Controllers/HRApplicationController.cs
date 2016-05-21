8eb3637e12038c87871461e7f94f00f07e0f8f02
Rahadian D Dewandono <gusdewa@gmail.com>
2016-05-21 20:13:15 +0700

    Model Binder for Date inside Grid

--- MCAWebAndAPI.Web/Controllers/HRApplicationController.cs
+++ MCAWebAndAPI.Web/Controllers/HRApplicationController.cs
@@ -22,22 +22,66 @@ namespace MCAWebAndAPI.Web.Controllers
 
 
         [HttpPost]
-        public ActionResult CreateApplicationData(ApplicationDataVM viewModel)
+        public ActionResult CreateApplicationData(FormCollection form, ApplicationDataVM viewModel)
         {
             var siteUrl = SessionManager.Get<string>("SiteUrl");
             _service.SetSiteUrl(siteUrl ?? ConfigResource.DefaultHRSiteUrl);
 
             var headerID = _service.CreateApplicationData(viewModel);
 
+            viewModel.EducationDetails = BindEducationDetails(form, viewModel.EducationDetails);
             _service.CreateEducationDetails(headerID, viewModel.EducationDetails);
+
+            viewModel.TrainingDetails = BindTrainingDetails(form, viewModel.TrainingDetails);
             _service.CreateTrainingDetails(headerID, viewModel.TrainingDetails);
+
+            viewModel.WorkingExperienceDetails = BindWorkingExperienceDetails(form, viewModel.WorkingExperienceDetails);
             _service.CreateWorkingExperienceDetails(headerID, viewModel.WorkingExperienceDetails);
+
             _service.CreateProfessionalDocuments(headerID, viewModel.Documents);
 
             return Json(new { status = true, urlToRedirect = siteUrl + ConfigResource.UrlApplication }, 
                 JsonRequestBehavior.AllowGet);
         }
 
+        private IEnumerable<WorkingExperienceDetailVM> BindWorkingExperienceDetails(FormCollection form, IEnumerable<WorkingExperienceDetailVM> workingExperienceDetails)
+        {
+            var array = workingExperienceDetails.ToArray();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i].From = BinderHelper.BindDateInGrid("WorkingExperienceDetails",
+                    i, "From", form);
+                array[i].To = BinderHelper.BindDateInGrid("WorkingExperienceDetails",
+                    i, "To", form);
+            }
+
+            return array;
+        }
+
+        private IEnumerable<TrainingDetailVM> BindTrainingDetails(FormCollection form, IEnumerable<TrainingDetailVM> trainingDetails)
+        {
+            var array = trainingDetails.ToArray();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i].Year = BinderHelper.BindDateInGrid("TrainingDetails",
+                    i, "Year", form);
+            }
+
+            return array;
+        }
+
+        private IEnumerable<EducationDetailVM> BindEducationDetails(FormCollection form, 
+            IEnumerable<EducationDetailVM> educationDetails)
+        {
+            var array = educationDetails.ToArray();
+            for (int i = 0; i< array.Length; i++)
+            {
+                array[i].YearOfGraduation = BinderHelper.BindDateInGrid("EducationDetails",
+                    i, "YearOfGraduation", form);
+            }
+
+            return array;
+        }
 
         public ActionResult CreateApplicationData(string siteUrl = null)
         {
---
+++ MCAWebAndAPI.Web/Helpers/BinderHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MCAWebAndAPI.Web.Helpers
+{
+    public class BinderHelper
+    {
+        public static DateTime? BindDateInGrid(string prefix, int index, string postfix, FormCollection form)
+        {
+            var dateStrings = (form[string.Format("{0}[{1}].{2}", prefix, index, postfix)] + string.Empty).Split(' ');
+
+            //"Mon Nov 21 2011 19:53:08 GMT+0700 (SE Asia Standard Time) -> Nov 21 2011"
+            var dateString = string.Format("{0} {1} {2}", dateStrings[1], dateStrings[2], dateStrings[3]);
+
+            var result = DateTime.ParseExact(dateString, "MMM dd yyyy",
+                                       System.Globalization.CultureInfo.InvariantCulture);
+            return result;
+        }
+    }
+}
