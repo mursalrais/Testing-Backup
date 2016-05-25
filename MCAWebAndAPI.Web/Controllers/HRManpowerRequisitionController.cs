73311b0f6f437a624190f8dca5bffc42c2b928d1
yaya123 <anugerah.septian@dac-solution.com>
2016-05-25 10:14:08 +0700

    Manpower Requisition Checkpoint 1

---
+++ MCAWebAndAPI.Web/Controllers/HRManpowerRequisitionController.cs
@@ -0,0 +1,24 @@
+﻿using MCAWebAndAPI.Service.HR.Common;
+using MCAWebAndAPI.Service.HR.Recruitment;
+using MCAWebAndAPI.Web.Filters;
+using MCAWebAndAPI.Web.Helpers;
+using MCAWebAndAPI.Web.Resources;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MCAWebAndAPI.Web.Controllers
+{
+    [Filters.HandleError]
+    public class HRManpowerRequisitionController : Controller
+    {
+        // GET: ManpowerRequisition
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
