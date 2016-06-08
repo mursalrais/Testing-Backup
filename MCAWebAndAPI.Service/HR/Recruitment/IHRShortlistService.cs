f5958b5a612da61d5e3a0922a550fe47eaf2a0c5
ahmadsobari <ahmadsobari89@gmail.com>
2016-06-08 14:51:17 +0700

    HR Shortlist by sobari

---
+++ MCAWebAndAPI.Service/HR/Recruitment/IHRShortlistService.cs
@@ -0,0 +1,21 @@
+﻿using MCAWebAndAPI.Model.HR.DataMaster;
+using MCAWebAndAPI.Model.ViewModel.Form.HR;
+using System.Collections.Generic;
+using System.Web;
+
+namespace MCAWebAndAPI.Service.HR.Recruitment
+{
+    public interface IHRShortlistService
+    {
+        void SetSiteUrl(string siteUrl = null);
+
+
+        IEnumerable<ApplicationShortlistVM> GetShortlists();
+
+        ApplicationShortlistVM GetShortlist(int? ID);
+
+        int? EditShortlistData(ShortlistDetailVM viewModel);
+
+        void CreateShortlistDataDetail(int? headerID, IEnumerable<ShortlistDetailVM> educationDetails);
+    }
+}
