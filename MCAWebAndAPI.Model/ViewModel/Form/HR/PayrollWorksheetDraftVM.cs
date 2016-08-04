9d09b172ea2b5e29f7aecbc96c11f1407ed87553
Rahadian Dewandono <gusdewa@gmail.com>
2016-08-04 17:40:17 +0700

    Display list of payroll worksheet drafts

---
+++ MCAWebAndAPI.Model/ViewModel/Form/HR/PayrollWorksheetDraftVM.cs
@@ -0,0 +1,21 @@
+﻿using MCAWebAndAPI.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCAWebAndAPI.Model.ViewModel.Form.HR
+{
+    public class PayrollWorksheetDraftVM : Item
+    {
+        public string FileName { get; set; }
+
+        public DateTime Period { get; set; }
+
+        public DateTime RunOn { get; set; }
+
+        public string UrlToDownload { get; set; }
+
+    }
+}
