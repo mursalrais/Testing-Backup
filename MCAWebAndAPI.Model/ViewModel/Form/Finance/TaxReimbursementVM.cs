424228b430ae7a48c4ebb3f80d08ee5ed43e2842
prumantir <perry.rumantir@gmail.com>
2016-08-04 10:15:47 +0700

    Added FIN files which were (for unknown reasons) ignored in pprevious commit

---
+++ MCAWebAndAPI.Model/ViewModel/Form/Finance/TaxReimbursementVM.cs
@@ -0,0 +1,104 @@
+﻿using MCAWebAndAPI.Model.Common;
+using MCAWebAndAPI.Model.ViewModel.Control;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCAWebAndAPI.Model.ViewModel.Form.Finance
+{
+    public class TaxReimbursementVM : Item
+    {
+        //TODO: lengkapi DisplayName, UIHint
+
+        ////public enum TaxType { Income = 1, VAT = 2, Others = 3 }
+        public enum CategoryType { Vendor, MCAI }
+
+        private const string TaxTypeIncomeTax = "Income Tax";
+        private const string TaxTypeVAT = "VAT";
+        private const string TaxTypeOthers = "others";
+
+        ////private Dictionary<TaxType, string> taxChoices = new Dictionary<TaxType, string>();
+
+
+        private ComboBoxVM _type;
+
+        ////public TaxReimbursementVM()
+        ////{
+        ////    this.taxChoices.Add(TaxType.Income, TaxTypeIncomeTax);
+        ////    this.taxChoices.Add(TaxType.VAT, TaxTypeVAT);
+        ////    this.taxChoices.Add(TaxType.Others, TaxTypeOthers);
+        ////}
+
+        [Required]
+        [DisplayName("Type of Tax")]
+        [UIHint("ComboBox")]
+        public ComboBoxVM Type
+        {
+            get
+            {
+                if (_type == null)
+                    _type = new ComboBoxVM()
+                    {
+                        Choices = new string[]
+                        {
+                            TaxTypeIncomeTax,
+                            TaxTypeVAT,
+                            TaxTypeOthers
+                        }
+                    };
+                return _type;
+            }
+            set
+            {
+                _type = value;
+            }
+        }
+
+        [Required]
+        [DisplayName("Letter No.")]
+        public string LetterNo { get; set; }
+
+        [Required]
+        [UIHint("Date")]
+        public DateTime LetterDate { get; set; }
+
+        [Required]
+        [UIHint("ComboBox")]
+        public string Category { get; set; }
+
+        [Required]
+        //[UIHint("ComboBox")]
+        public int ContractorId { get; set; }
+
+        [Required]
+        public string ContractorName { get; set; }
+
+        [Required]
+        public string Object { get; set; }
+
+        [Required]
+        [DisplayName("Tax Peiod")]
+        [UIHint("Date")]
+        public DateTime Period { get; set; }
+
+        [Required]
+        public decimal AmountIDR { get; set; }
+
+        [Required]
+        [UIHint("TextArea")]
+        [DataType(DataType.MultilineText)]
+        public string Remarks { get; set; }
+
+        [Required]
+        [UIHint("Date")]
+        public DateTime PaymentReceivedDate { get; set; }
+
+        public string DocumentNo { get; set; }
+
+        //TODO: attachment
+    }
+}
---
+++ MCAWebAndAPI.Service/Finance/TaxReimbursementService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MCAWebAndAPI.Model.ViewModel.Form.Finance;
+
+namespace MCAWebAndAPI.Service.Finance
+{
+    public class TaxReimbursementService : ITaxReimbursementService
+    {
+        string _siteUrl = null;
+
+        public TaxReimbursementVM GetTaxReimbursement(int? id = default(int?))
+        {
+            return new TaxReimbursementVM();
+        }
+
+        public void SetSiteUrl(string siteUrl)
+        {
+            _siteUrl = siteUrl;
+        }
+
+
+    }
+}
