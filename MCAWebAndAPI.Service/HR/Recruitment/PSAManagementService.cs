ca0b37cb0e74733f105b0a1d0498245a91ba1dd0
mursalrais <mursalrais@gmail.com>
2016-08-05 14:22:43 +0700

    PSAManagementVM IPSAManagementService PSAManagementService PSAManagementJob PSAManagementScheduler IPSAScheduleService PSAScheduleService HRPSAManagementController CreatePSAManagement TestCalculateTwoMonths _PSAManagmentData

--- MCAWebAndAPI.Service/HR/Recruitment/PSAManagementService.cs
+++ MCAWebAndAPI.Service/HR/Recruitment/PSAManagementService.cs
@@ -49,7 +49,10 @@ namespace MCAWebAndAPI.Service.HR.Recruitment
             viewModel.PSAId = Convert.ToInt32(item["ID"]);
             viewModel.DateOfNewPSABefore = Convert.ToDateTime(item["dateofnewpsa"]).ToLocalTime();
             viewModel.DateNewPSABefore = Convert.ToDateTime(item["dateofnewpsa"]).ToLocalTime().ToShortDateString();
-            viewModel.ProfessionalMail = Convert.ToString((item["Professional_x0020_Name_x003a_Of"] as FieldLookupValue).LookupValue);
+
+            var professionalData = SPConnector.GetListItem(SP_PROF_LIST_NAME, viewModel.ID, _siteUrl);
+            viewModel.ProfessionalMail = Convert.ToString(professionalData["officeemail"]);
+
             viewModel.StrPSARenewal = Convert.ToString(item["renewalnumber"]);
             viewModel.ProjectUnit = Convert.ToString(item["ProjectOrUnit"]);
             viewModel.PositionID = Convert.ToInt32((item["position"] as FieldLookupValue).LookupId);
@@ -115,6 +118,8 @@ namespace MCAWebAndAPI.Service.HR.Recruitment
             //updatedValues.Add("lastworkingdate", psaManagement.HiddenExpiryDate);
             
             updatedValues.Add("lastworkingdate", psaManagement.LastWorkingDate);
+
+
 
 
             try
@@ -298,6 +303,35 @@ namespace MCAWebAndAPI.Service.HR.Recruitment
             return true;
         }
 
+        public bool UpdateProfessionalFromPSA(PSAManagementVM psaManagement, int? psaID)
+        {
+            var updateValues = new Dictionary<string, object>();
+
+            var psaData = SPConnector.GetListItem(SP_PSA_LIST_NAME, psaID, _siteUrl);
+            string psaNumber = Convert.ToString(psaData["Title"]);
+
+            updateValues.Add("Join_x0020_Date", psaManagement.JoinDate.Value);
+            updateValues.Add("ProjectOrUnit", psaManagement.ProjectOrUnit.Value);
+            updateValues.Add("position", new FieldLookupValue { LookupId = Convert.ToInt32(psaManagement.Position.Value) });
+            updateValues.Add("PSAnumber", psaNumber);
+            updateValues.Add("PSAstartdate", psaManagement.DateOfNewPSA.Value);
+            updateValues.Add("PSAexpirydate", psaManagement.PSAExpiryDate.Value);
+                        
+            try
+            {
+                SPConnector.UpdateListItem(SP_PROF_LIST_NAME, psaManagement.ID, updateValues, _siteUrl);
+            }
+            catch (Exception e)
+            {
+                logger.Debug(e.Message);
+                return false;
+            }
+
+            var entitiy = new PSAManagementVM();
+            entitiy = psaManagement;
+            return true;
+        }
+
         private IEnumerable<HttpPostedFileBase> GetDocuments(int? iD)
         {
             var caml = @"<View>  
@@ -340,9 +374,20 @@ namespace MCAWebAndAPI.Service.HR.Recruitment
             }
         }
 
-        public void SendMailPerformancePlan(string professionalMail, string mailContent)
+        public void SendMailPerformancePlan(int? professionalID, string siteUrl, DateTime today)
         {
-            EmailUtil.Send(professionalMail, "Create Performance Plan", mailContent);
+            var currentYear = today.Year;
+            //var calculateLastYear = today.AddYears(-1);
+            //var lastYear = calculateLastYear.Year;
+
+            var professionalData = SPConnector.GetListItem(SP_PROF_LIST_NAME, professionalID, _siteUrl);
+            string professionalMail = Convert.ToString(professionalData["officeemail"]);
+            string professionalFullName = Convert.ToString(professionalData["Title"]) + " " + Convert.ToString(professionalData["lastname"]);
+
+            string mailSubject = string.Format("Initiation Performance Plan for Period {0}", currentYear);
+            string mailContent = string.Format("Dear Mr./Mrs. {0}, This email is sent to you to notify that you are required to create Performance Plan for period {1}. Creating and approval plan process will take maximum 5 working days. Therefore, do prepare your plan accordingly. To Create the performance plan, please click the following link: {2}{3}/NewForm_Custom.aspx", professionalFullName, currentYear, siteUrl, UrlResource.ProfessionalPerformancePlan);
+
+            EmailUtil.Send(professionalMail, mailSubject, mailContent);
         }
 
         public string GetProfessionalFullName(int? professionalID)
@@ -352,6 +397,50 @@ namespace MCAWebAndAPI.Service.HR.Recruitment
             string professionalFullName = Convert.ToString(item["Title"]) + " " + Convert.ToString(item["lastname"]);
 
             return professionalFullName;
+        }
+
+        public int GetPSALatestID(string _siteUrl)
+        {
+            int psaID = SPConnector.GetLatestListItemID(SP_PSA_LIST_NAME, _siteUrl, null);
+
+            return psaID;
+        }
+
+        public bool UpdateStatusPSABefore(int? psaRenewalNumberMinusOne, string professionalName)
+        {
+            var camlPSABefore = @"<View>  
+            <Query> 
+               <Where><And><Eq><FieldRef Name='professional' /><Value Type='Lookup'>" + professionalName + @"</Value></Eq><Eq><FieldRef Name='renewalnumber' /><Value Type='Number'>" + psaRenewalNumberMinusOne + @"</Value></Eq></And></Where> 
+            </Query> 
+      </View>";
+
+            int psaID = SPConnector.GetLatestListItemID(SP_PSA_LIST_NAME, _siteUrl, camlPSABefore);
+
+            var updateValues = new Dictionary<string, object>();
+            string psaStatus = "In Active";
+
+            updateValues.Add("psastatus", psaStatus);
+
+            try
+            {
+                SPConnector.UpdateListItem(SP_PSA_LIST_NAME, psaID, updateValues, _siteUrl);
+            }
+            catch (Exception e)
+            {
+                logger.Debug(e.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        public string GetProfessionalName(int? professionalID)
+        {
+            var professionalData = SPConnector.GetListItem(SP_PROF_LIST_NAME, professionalID, _siteUrl);
+
+            string professionalName = Convert.ToString(professionalData["Title"]);
+
+            return professionalName;
         }
     }
 }
---
+++ MCAWebAndAPI.Service/JobSchedulers/Schedulers/PSAManagementScheduler.cs
@@ -0,0 +1,40 @@
+﻿using MCAWebAndAPI.Service.JobSchedulers.Jobs;
+using NLog;
+using Quartz;
+using Quartz.Impl;
+using System;
+
+namespace MCAWebAndAPI.Service.JobSchedulers.Schedulers
+{
+    public class PSAManagementScheduler
+    {
+        static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static void DoNow_OnceEveryDay(int? psaID, string siteUrl = null)
+        {
+            // construct a scheduler factory
+            ISchedulerFactory scheduleFactory = new StdSchedulerFactory();
+
+            // get a scheduler
+            IScheduler scheduler = scheduleFactory.GetScheduler();
+            scheduler.Start();
+            logger.Debug(string.Format("{0} has been started at {1} in site {2}",
+                scheduler.SchedulerName, DateTime.Now.ToLongDateString(), siteUrl));
+
+            IJobDetail job = JobBuilder.Create<PSAManagementJob>()
+                .WithIdentity("calculate-task-insite-" + siteUrl)
+                .UsingJobData("site-url", siteUrl) // passing variable
+                .Build();
+
+            // Trigger the job to run now, and then every 1 hour
+            ITrigger trigger = TriggerBuilder.Create()
+              .WithIdentity("start-now-per-day-insite-" + siteUrl, "repetitive-triggers")
+              .StartNow() // start when?
+              .WithSimpleSchedule(x => x
+                  .WithIntervalInHours(24)) // interval or how often?
+              .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+        }
+    }
+}
