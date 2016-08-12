75281c072a483d8d867e281ff36789c8aeac70c3
ahmadsobari <ahmadsobari89@gmail.com>
2016-08-12 13:20:36 +0700

    start progress HR14 HRAdjustment

---
+++ MCAWebAndAPI.Service/HR/Payroll/AdjustmentService.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using MCAWebAndAPI.Model.ViewModel.Form.HR;
+using MCAWebAndAPI.Service.Utils;
+using NLog;
+using Microsoft.SharePoint.Client;
+using MCAWebAndAPI.Service.Resources;
+using MCAWebAndAPI.Model.Common;
+using MCAWebAndAPI.Model.HR.DataMaster;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace MCAWebAndAPI.Service.HR.Payroll
+{
+    public class AdjustmentService : IAdjustmentService
+    {
+        string _siteUrl;
+        static Logger logger = LogManager.GetCurrentClassLogger();
+
+        const string SP_AJUDATA_LIST_NAME = "Ajustment";
+        const string SP_PROMAS_LIST_NAME = "Professional Master";
+
+        public void SetSiteUrl(string siteUrl = null)
+        {
+            _siteUrl = FormatUtil.ConvertToCleanSiteUrl(siteUrl);
+        }
+
+        public AdjustmentDataVM GetAjusmentData(string period)
+        {
+            var viewModel = new AdjustmentDataVM();
+
+            if (period == null)
+                return viewModel;
+
+            viewModel.ajustmentDetails = GetAjusmentDetails(period);
+
+            return viewModel;
+        }
+
+        //<ViewFields>
+        //   <FieldRef Name = 'Title' />
+        //   < FieldRef Name='applications' />
+        //   <FieldRef Name = 'university' />
+        //   < FieldRef Name='yearofgraduation' />
+        //   <FieldRef Name = 'remarks' />
+        //</ ViewFields >
+        private IEnumerable<AdjustmentDetailsVM> GetAjusmentDetails(string period)
+        {
+            var caml = @"<View>  
+                              <Query> 
+                            <Where>
+                              <Eq>
+                                 <FieldRef Name='adjustmentperiod' />
+                                 <Value IncludeTimeValue='TRUE' Type='DateTime'>" + period + @"</Value>
+                              </Eq>
+                            </Where>
+                                </Query> 
+                               <Query />
+                            <ViewFields>
+                                <FieldRef Name='ID' />
+                                <FieldRef Name='ContentType' />
+                                <FieldRef Name='Title' />
+                                <FieldRef Name='adjustmentperiod' />
+                                <FieldRef Name='professional' />
+                                <FieldRef Name='professional_x003a_ID' />
+                                <FieldRef Name='adjustmenttype' />
+                                <FieldRef Name='adjustmentamount' />
+                                <FieldRef Name='adjustmentcurrency' />
+                                <FieldRef Name='debitorcredit' />
+                                <FieldRef Name='remarks' />
+                            </ViewFields>
+                           </View>";
+
+            var adjustmentDetails = new List<AdjustmentDetailsVM>();
+
+            foreach (var item in SPConnector.GetList(SP_AJUDATA_LIST_NAME, _siteUrl, caml))
+            {
+                adjustmentDetails.Add(ConvertToAdjusDetailVM(item));
+            }
+
+            return adjustmentDetails;
+        }
+
+        private AdjustmentDetailsVM ConvertToAdjusDetailVM(ListItem item)
+        {
+            return new AdjustmentDetailsVM
+            {
+                ajusmentType = AdjustmentDetailsVM.getAjusmentDefaultValue(
+                    new Model.ViewModel.Control.AjaxComboBoxVM
+                    {
+                        Text = Convert.ToString(item["adjustmenttype"]),
+                    }),
+
+                currency = AdjustmentDetailsVM.getCurrencyDefaultValue(
+                    new Model.ViewModel.Control.AjaxComboBoxVM
+                    {
+                        Text = Convert.ToString(item["adjustmentcurrency"]),
+                    }),
+
+                payType = AdjustmentDetailsVM.getpayTypeDefaultValue(
+                    new Model.ViewModel.Control.AjaxComboBoxVM
+                    {
+                        Text = Convert.ToString(item["debitorcredit"]),
+                    }),
+
+                ddlProfessional = AdjustmentDetailsVM.getpayTypeDefaultValue(
+                    new Model.ViewModel.Control.AjaxComboBoxVM
+                    {
+                        ValueField = Convert.ToString(item["professional_x003a_ID"]),
+                    }),
+
+                remark = Convert.ToString(item["remarks"]),
+                Title = Convert.ToString(item["Title"]),
+                ID = Convert.ToInt32(item["ID"]),
+            };
+        }
+
+        public void CreateAdjustmentData(string period, AdjustmentDataVM viewModels)
+        {
+            foreach (var viewModel in viewModels.ajustmentDetails)
+            {
+                if (viewModel.ID == null)
+                {
+                    var cratedValueDetail = new Dictionary<string, object>();
+
+                    cratedValueDetail.Add("Title", viewModel.Title);
+                    cratedValueDetail.Add("adjustmentperiod", period);
+                    cratedValueDetail.Add("professional_x003a_ID", viewModel.profId);
+                    cratedValueDetail.Add("adjustmenttype", viewModel.ajusmentType);
+                    cratedValueDetail.Add("adjustmentamount", viewModel.amount);
+                    cratedValueDetail.Add("adjustmentcurrency", viewModel.currency);
+                    cratedValueDetail.Add("debitorcredit", viewModel.payType);
+                    cratedValueDetail.Add("remarks", viewModel.remark);
+
+                    try
+                    {
+                        SPConnector.AddListItem(SP_AJUDATA_LIST_NAME, cratedValueDetail, _siteUrl);
+                    }
+                    catch (Exception e)
+                    {
+                        logger.Error(e.Message);
+                        throw e;
+                    }
+                }
+                else
+                {
+                    var updatedValue = new Dictionary<string, object>();
+
+                    updatedValue.Add("Title", viewModel.Title);
+                    updatedValue.Add("adjustmentperiod", period);
+                    updatedValue.Add("professional_x003a_ID", viewModel.profId);
+                    updatedValue.Add("adjustmenttype", viewModel.ajusmentType);
+                    updatedValue.Add("adjustmentamount", viewModel.amount);
+                    updatedValue.Add("adjustmentcurrency", viewModel.currency);
+                    updatedValue.Add("debitorcredit", viewModel.payType);
+                    updatedValue.Add("remarks", viewModel.remark);
+
+                    try
+                    {
+                        SPConnector.UpdateListItem(SP_AJUDATA_LIST_NAME, viewModel.ID, updatedValue, _siteUrl);
+
+                    }
+                    catch (Exception e)
+                    {
+                        logger.Error(e.Message);
+                        throw e;
+                    }
+                }
+                if (Item.CheckIfDeleted(viewModel))
+                {
+                    try
+                    {
+                        SPConnector.DeleteListItem(SP_AJUDATA_LIST_NAME, viewModel.ID, _siteUrl);
+                    }
+                    catch (Exception e)
+                    {
+                        logger.Error(e);
+                        throw e;
+                    }
+                    continue;
+                }
+            }
+        }
+    }
+}
---
+++ MCAWebAndAPI.Web/Controllers/HRAdjustmentController.cs
@@ -0,0 +1,95 @@
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using MCAWebAndAPI.Model.ViewModel.Form.HR;
+using MCAWebAndAPI.Service.HR.Payroll;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using MCAWebAndAPI.Model.ViewModel.Control;
+using MCAWebAndAPI.Web.Filters;
+using MCAWebAndAPI.Web.Resources;
+using MCAWebAndAPI.Web.Helpers;
+using MCAWebAndAPI.Service.Resources;
+using System.Net;
+using System;
+using System.IO;
+using MCAWebAndAPI.Service.Converter;
+using Elmah;
+using System.Threading.Tasks;
+using System.Web;
+using MCAWebAndAPI.Service.Utils;
+
+namespace MCAWebAndAPI.Web.Controllers
+{
+    [Filters.HandleError]
+
+    public class HRAdjustmentController : Controller
+    {
+        IAdjustmentService _service;
+
+        const string SP_TRANSACTION_WORKFLOW_LOOKUP_COLUMN_NAME = "compensatoryrequest";
+
+        public HRAdjustmentController()
+        {
+            _service = new AdjustmentService();
+        }
+
+        public ActionResult InputAjustmentData (string siteurl = null, string period = null)
+        {
+            var viewmodel = new AdjustmentDataVM();
+
+            //mandatory: set site url
+            _service.SetSiteUrl(siteurl ?? ConfigResource.DefaultHRSiteUrl);
+            SessionManager.Set("SiteUrl", siteurl ?? ConfigResource.DefaultHRSiteUrl);
+
+            if (siteurl == "")
+            {
+                siteurl = SessionManager.Get<string>("SiteUrl");
+            }
+            _service.SetSiteUrl(siteurl ?? ConfigResource.DefaultHRSiteUrl);
+
+            viewmodel = _service.GetAjusmentData(period);
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateAdjustmentData(FormCollection form, AdjustmentDataVM viewModel)
+        {
+            var siteUrl = SessionManager.Get<string>("SiteUrl");
+            _service.SetSiteUrl(siteUrl ?? ConfigResource.DefaultHRSiteUrl);
+
+            string period = viewModel.periodDate;
+
+            try
+            {
+                _service.CreateAdjustmentData(period, viewModel);
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return JsonHelper.GenerateJsonErrorResponse(e);
+            }
+
+            return JsonHelper.GenerateJsonSuccessResponse(siteUrl + UrlResource.Compensatory);
+
+        }
+
+        public async Task<ActionResult> GetAdjustmentDetails(string period)
+        {
+            var viewmodel = new AdjustmentDataVM();
+
+            if (period == null)
+                return PartialView("_InputCompensantoryDetails", viewmodel.ajustmentDetails);
+
+            var siteUrl = SessionManager.Get<string>("SiteUrl");
+            _service.SetSiteUrl(siteUrl ?? ConfigResource.DefaultHRSiteUrl);
+
+            viewmodel = _service.GetAjusmentData(period);
+
+            return PartialView("_InputCompensantoryDetails", viewmodel.ajustmentDetails);
+        }
+
+    }
+}
