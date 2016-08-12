428e5522cf6c38d40a675707e30b97ecfa90ceb6
arfanan <siti.marfuah@dac-solution.com>
2016-08-12 14:07:44 +0700

    disabliing field am04

--- MCAWebAndAPI.Model/ViewModel/Form/Asset/AssignmentofAssetDetailVM.cs
+++ MCAWebAndAPI.Model/ViewModel/Form/Asset/AssignmentofAssetDetailVM.cs
@@ -12,145 +12,218 @@ namespace MCAWebAndAPI.Model.ViewModel.Form.Asset
 {
     public class AssignmentofAssetDetailVM : Item
     {
-        private ComboBoxVM _assetSubAsset, _province, _office, _floor, _room;
+        //private ComboBoxVM _assetSubAsset, _province, _office, _floor, _room;
 
         public int Id { get; set; }
 
         [DisplayName("Asset-Sub Asset")]
-        [UIHint("ComboBox")]
-        public ComboBoxVM AssetSubAsset
-        {
-            get
-            {
-                if (_assetSubAsset == null)
-                {
-                    _assetSubAsset = new ComboBoxVM()
-                    {
-                        Choices = new string[]
-                        {
-                            "FXA-GP-LA-0001- Server Machine",
-                            "FXA-GP-LA-0001- Server Machine",
-                            "FXA-GP-LA-0001- Server Machine"
-                        }
-                    };
-                }
-                return _assetSubAsset;
-            }
+        [UIHint("InGridAjaxComboBox")]
+        public AjaxComboBoxVM AssetSubAsset { get; set; } = new AjaxComboBoxVM();
 
-            set
+        public static AjaxComboBoxVM GetAssetSubAssetDefaultValue(AjaxComboBoxVM model = null)
+        {
+            if (model == null)
             {
-                _assetSubAsset = value;
+                return new AjaxComboBoxVM();
+            }
+            else
+            {
+                return model;
             }
         }
 
-        [UIHint("ComboBox")]
-        public ComboBoxVM Province
-        {
-            get
-            {
-                if (_province == null)
-                {
-                    _province = new ComboBoxVM()
-                    {
-                        Choices = new string[]
-                        {
-                            "Jawa Timur",
-                            "Jawa Barat",
-                            "Jawa Tengah"
-                        }
-                    };
-                }
-                return _province;
-            }
+        [UIHint("InGridAjaxComboBox")]
+        public AjaxComboBoxVM Province { get; set; } = new AjaxComboBoxVM();
 
-            set
+        public static AjaxComboBoxVM GetProvinceDefaultValue(AjaxComboBoxVM model = null)
+        {
+            if (model == null)
             {
-                _province = value;
+                return new AjaxComboBoxVM();
+            }
+            else
+            {
+                return model;
             }
         }
 
-        [UIHint("ComboBox")]
-        public ComboBoxVM Office
-        {
-            get
-            {
-                if (_office == null)
-                {
-                    _office = new ComboBoxVM()
-                    {
-                        Choices = new string[]
-                        {
-                            "Gedung MR21",
-                            "Eceos"
-                        }
-                    };
-                }
-                return _office;
-            }
+        [UIHint("InGridAjaxComboBox")]
+        public AjaxComboBoxVM Office { get; set; } = new AjaxComboBoxVM();
 
-            set
+        public static AjaxComboBoxVM GetOfficeDefaultValue(AjaxComboBoxVM model = null)
+        {
+            if (model == null)
             {
-                _office = value;
+                return new AjaxComboBoxVM();
+            }
+            else
+            {
+                return model;
             }
         }
 
-        [UIHint("ComboBox")]
-        public ComboBoxVM Floor
-        {
-            get
-            {
-                if (_floor == null)
-                {
-                    _floor = new ComboBoxVM()
-                    {
-                        Choices = new string[]
-                        {
-                            "11",
-                            "12",
-                            "13"
-                        }
-                    };
-                }
-                return _floor;
-            }
+        [UIHint("InGridAjaxComboBox")]
+        public AjaxComboBoxVM Floor { get; set; } = new AjaxComboBoxVM();
 
-            set
+        public static AjaxComboBoxVM GetFloorDefaultValue(AjaxComboBoxVM model = null)
+        {
+            if (model == null)
             {
-                _floor = value;
+                return new AjaxComboBoxVM();
+            }
+            else
+            {
+                return model;
             }
         }
 
-        [UIHint("ComboBox")]
-        public ComboBoxVM Room
-        {
-            get
-            {
-                if (_room == null)
-                {
-                    _room = new ComboBoxVM()
-                    {
-                        Choices = new string[]
-                        {
-                            "A",
-                            "B",
-                            "C"
-                        }
-                    };
-                }
-                return _room;
-            }
+        [UIHint("InGridAjaxComboBox")]
+        public AjaxComboBoxVM Room { get; set; } = new AjaxComboBoxVM();
 
-            set
+        public static AjaxComboBoxVM GetRoomDefaultValue(AjaxComboBoxVM model = null)
+        {
+            if (model == null)
             {
-                _room = value;
+                return new AjaxComboBoxVM();
+            }
+            else
+            {
+                return model;
             }
         }
+
+        //[DisplayName("Asset-Sub Asset")]
+        //[UIHint("ComboBox")]
+        //public ComboBoxVM AssetSubAsset
+        //{
+        //    get
+        //    {
+        //        if (_assetSubAsset == null)
+        //        {
+        //            _assetSubAsset = new ComboBoxVM()
+        //            {
+        //                Choices = new string[]
+        //                {
+        //                    "FXA-GP-LA-0001- Server Machine",
+        //                    "FXA-GP-LA-0001- Server Machine",
+        //                    "FXA-GP-LA-0001- Server Machine"
+        //                }
+        //            };
+        //        }
+        //        return _assetSubAsset;
+        //    }
+
+        //    set
+        //    {
+        //        _assetSubAsset = value;
+        //    }
+        //}
+
+        //[UIHint("ComboBox")]
+        //public ComboBoxVM Province
+        //{
+        //    get
+        //    {
+        //        if (_province == null)
+        //        {
+        //            _province = new ComboBoxVM()
+        //            {
+        //                Choices = new string[]
+        //                {
+        //                    "Jawa Timur",
+        //                    "Jawa Barat",
+        //                    "Jawa Tengah"
+        //                }
+        //            };
+        //        }
+        //        return _province;
+        //    }
+
+        //    set
+        //    {
+        //        _province = value;
+        //    }
+        //}
+
+        //[UIHint("ComboBox")]
+        //public ComboBoxVM Office
+        //{
+        //    get
+        //    {
+        //        if (_office == null)
+        //        {
+        //            _office = new ComboBoxVM()
+        //            {
+        //                Choices = new string[]
+        //                {
+        //                    "Gedung MR21",
+        //                    "Eceos"
+        //                }
+        //            };
+        //        }
+        //        return _office;
+        //    }
+
+        //    set
+        //    {
+        //        _office = value;
+        //    }
+        //}
+
+        //[UIHint("ComboBox")]
+        //public ComboBoxVM Floor
+        //{
+        //    get
+        //    {
+        //        if (_floor == null)
+        //        {
+        //            _floor = new ComboBoxVM()
+        //            {
+        //                Choices = new string[]
+        //                {
+        //                    "11",
+        //                    "12",
+        //                    "13"
+        //                }
+        //            };
+        //        }
+        //        return _floor;
+        //    }
+
+        //    set
+        //    {
+        //        _floor = value;
+        //    }
+        //}
+
+        //[UIHint("ComboBox")]
+        //public ComboBoxVM Room
+        //{
+        //    get
+        //    {
+        //        if (_room == null)
+        //        {
+        //            _room = new ComboBoxVM()
+        //            {
+        //                Choices = new string[]
+        //                {
+        //                    "A",
+        //                    "B",
+        //                    "C"
+        //                }
+        //            };
+        //        }
+        //        return _room;
+        //    }
+
+        //    set
+        //    {
+        //        _room = value;
+        //    }
+        //}
 
         public string Remarks { get; set; }
         public string Status { get; set; }
 
-        public string AssetDescription { get; set; }
-        public string Item { get; set; }
-        public string NewAsset { get; set; }
     }
 }
