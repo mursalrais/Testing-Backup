1d61b11ea5428bf58d8ec16d230ce78073fd27b1
ahmadsobari <ahmadsobari89@gmail.com>
2016-08-16 14:37:31 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	MCAWebAndAPI.Model/MCAWebAndAPI.Model.csproj
    #	MCAWebAndAPI.Service/Shared/ActivityService.cs
    #	MCAWebAndAPI.Web/MCAWebAndAPI.Web.csproj

---
+++ MCAWebAndAPI.Model/ViewModel/Form/Finance/TaxExemptionOtherVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCAWebAndAPI.Model.ViewModel.Form.Finance
+{
+    public class TaxExemptionOtherVM : TaxExemptionBaseVM
+    {
+        [DisplayName("Gross Income (IDR)")]
+        public Decimal GrossIncome
+        {
+            get;
+            set;
+        }
+
+        [DisplayName("Total Tax (IDR)")]
+        public Decimal TotalTax
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ MCAWebAndAPI.Service/HR/AdjustmentDayOffBalance/IAdjustmentDayOffBalanceService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MCAWebAndAPI.Model.ViewModel.Form.HR;
+
+namespace MCAWebAndAPI.Service.HR.AdjustmentDayOffBalance
+{
+    public interface IAdjustmentDayOffBalanceService
+    {
+        void SetSiteUrl(string siteUrl);
+
+        string GetProfessionalName(int? professionalID);
+
+        AdjustmentDayOffBalanceVM GetAdjustmentDayOffBalance(int? iD);
+
+        int GetLastBalanceCompensatory(int? professionalID, string dayOffType);
+
+        int GetLastBalanceAnnualDayOff(int? professionalID, string dayOffType);
+
+        int GetLastBalanceSpecialDayOff(int? professionalID, string dayOffType);
+
+        int GetLastBalancePaternity(int? professionalID, string dayOffType);
+
+        int CreateAdjustmentDayOffBalance(AdjustmentDayOffBalanceVM adjustmentDayOffBalance);
+
+        bool UpdateAdjustmentDayOffBalance(AdjustmentDayOffBalanceVM adjustmentDayOffBalance);
+
+        bool UpdateAnnualDayOffBalance(AdjustmentDayOffBalanceVM adjustmentDayOffBalance);
+
+        bool UpdateSpecialDayOffBalance(AdjustmentDayOffBalanceVM adjustmentDayOffBalance);
+
+        bool UpdatePaternityBalance(AdjustmentDayOffBalanceVM adjustmentDayOffBalance);
+
+        bool UpdateCompensatoryBalance(AdjustmentDayOffBalanceVM adjustmentDayOffBalance);
+
+
+    }
+}
