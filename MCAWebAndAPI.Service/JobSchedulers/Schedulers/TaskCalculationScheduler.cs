682566d504e6582ca311609b1a70ea7b1b6b3878
yaya123 <anugerah.septian@dac-solution.com>
2016-09-02 17:59:06 +0700

    change status professional dynamicly

--- MCAWebAndAPI.Service/JobSchedulers/Schedulers/TaskCalculationScheduler.cs
+++ MCAWebAndAPI.Service/JobSchedulers/Schedulers/TaskCalculationScheduler.cs
@@ -39,5 +39,35 @@ namespace MCAWebAndAPI.Service.JobSchedulers.Schedulers
 
             scheduler.ScheduleJob(job, trigger);
         }
+
+        public static void EmailReminderDoNow_OnceEveryDay(string siteUrl)
+        {
+            // construct a scheduler factory
+            ISchedulerFactory scheduleFactory = new StdSchedulerFactory();
+
+            // get a scheduler
+            IScheduler scheduler = scheduleFactory.GetScheduler();
+            scheduler.Start();
+            logger.Debug(string.Format("{0} has been started at {1} in site {2}",
+                scheduler.SchedulerName, DateTime.Now.ToLongDateString(), siteUrl));
+
+            IJobDetail job = JobBuilder.Create<EmailReminder5Days>()
+                .WithIdentity("calculate-task-insite-" + siteUrl)
+                .UsingJobData("site-url", siteUrl) // passing variable
+                .Build();
+
+            // Trigger the job to run now, and then every 24 hours
+            ITrigger trigger = TriggerBuilder.Create()
+              .WithIdentity("start-now-per-day-insite-" + siteUrl, "repetitive-triggers")
+              .StartNow() // start when?
+              .WithSimpleSchedule(x => x
+                  .WithIntervalInHours(24)) // interval or how often?
+              .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+        }
     }
 }
+
+
+
