97e87921af15393753ad39caf5efb86d11978871
mursalrais <mursalrais@gmail.com>
2016-09-26 10:04:36 +0700

    Update URL Resource HR02

--- MCAWebAndAPI.Service/Resources/UrlResource.Designer.cs
+++ MCAWebAndAPI.Service/Resources/UrlResource.Designer.cs
@@ -106,7 +106,7 @@ namespace MCAWebAndAPI.Service.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to {0}/Application%20Documents/Forms/AllItems.aspx#InplviewHashc902afed-7be4-4799-9502-1818597c2223=FilterField1%3Dapplication%255Fx003a%255FID-FilterValue1%3D{1}.
+        ///   Looks up a localized string similar to {0}/Application%20Documents/Forms/AllItems.aspx#InplviewHash932a3d67-1171-4850-8195-3d025ae3b82e=FilterField1%3Dapplication%255Fx003a%255FID-FilterValue1%3D{1}.
         /// </summary>
         public static string ApplicationDocumentByID {
             get {
